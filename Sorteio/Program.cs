21aa46bf214665c2a41480ebd8448b1044ead25c
EukirneRas <henriquedeandraderodrigues@gmail.com>
2022-10-18 16:01:12 -0300

    Commit do Sorteio

---
+++ Sorteio/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Sorteio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randNum = new Random();
+            int x = randNum.Next();
+            int num;
+            bool f = true;
+            int valorInteiro = randNum.Next(0,100);
+
+            for (int i = 5; i > 0; i--)
+            {
+                
+                Console.WriteLine("Tente a sorte com seu número! De 0 a 100! Você tem " + i + " chance");
+                num = int.Parse(Console.ReadLine());
+
+                if (num > valorInteiro)
+                {
+                    Console.WriteLine("O valor inserido é maior que o valor sorteado");
+                    f = false;
+                }
+
+                if (num < valorInteiro)
+                {
+                    Console.WriteLine("O valor inserido é menor que o valor sorteado");
+                    f = false;
+                }
+                if (num == valorInteiro)
+                {
+                    i = 0;
+                    f = true;
+                    Console.WriteLine("Parabéns! Você acertou!");
+                }
+            }
+            if (f == false)
+            {
+                Console.WriteLine("Você Perdeu! O valor era: " + valorInteiro);
+            }
+        }
+    }
+}
